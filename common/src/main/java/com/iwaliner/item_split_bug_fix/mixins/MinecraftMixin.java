0802a27c490aa6a6bc3217f066fa9930628a636e
luoxueyasha <bing1273103582@outlook.com>
2025-09-14 15:24:21 +0800

    build success for 1.19.2 forge and 1.20.1 forge, not yet for 1.20.1 nf and 1.21.1 nf
    fxxk gradle I FINALLY MADE IT

--- common/src/main/java/com/iwaliner/item_split_bug_fix/mixins/MinecraftMixin.java
+++ common/src/main/java/com/iwaliner/item_split_bug_fix/mixins/MinecraftMixin.java
@@ -10,6 +10,17 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class MinecraftMixin {
     @Inject(method = "runTick", at = @At("HEAD"))
     private void init(boolean bl, CallbackInfo ci) {
-        System.out.println("runTick mixin!");
+        String str = "runTick mixin from Mymod!";
+        #if MC_VER == MC_1_19_2
+            str += "1.19.2";
+        #elif MC_VER == MC_1_20_1
+            str += "1.20.1";
+        #elif MC_VER == MC_1_21_1
+            str += "1.21.1";
+        #endif
+
+        System.out.println(str);
+
     }
+
 }
