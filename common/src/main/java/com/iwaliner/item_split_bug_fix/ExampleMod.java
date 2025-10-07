cafa1a871e5b6373eddb66f2592155c2c38bb0f4
luoxueyasha <bing1273103582@outlook.com>
2025-10-07 18:37:50 +0800

    backup. could not run

--- common/src/main/java/com/iwaliner/item_split_bug_fix/ExampleMod.java
+++
@@ -1,9 +0,0 @@
-package com.iwaliner.item_split_bug_fix;
-
-public class ExampleMod {
-    public static final String MOD_ID = "item_split_bug_fix";
-
-    public static void init() {
-        System.out.println("Hello World!");
-    }
-}
